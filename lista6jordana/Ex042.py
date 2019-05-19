1b51eee9e87140a8202c34f93d05a89b49822abc
AtilioA <atiliodadalto@hotmail.com>
2019-05-19 15:49:00 -0300

    Lista 6 feita

--- lista6jordana/Ex042.py
+++ lista6jordana/Ex042.py
@@ -1,3 +1,15 @@
 # 9) Defina uma função que ordene uma lista de forma crescente.
 
-    
+def permuta(lista, i1, i2):
+    aux = lista[i1]
+    lista[i1] = lista[i2]
+    lista[i2] = aux
+    return lista
+
+def bolhaSorte(lista):
+    for i, x in enumerate(lista):
+        if (i + 1) < len(lista):
+            if lista[i + 1] < x:
+                permuta(lista, i, i + 1)
+                bolhaSorte(lista)
+    return lista
--- lista6jordana/Ex044.py
+++ lista6jordana/Ex044.py
@@ -1,3 +1,14 @@
 # 11) Dada uma string (lista de caracteres), verificar se é um palíndromo (a string é a mesma
 # quando lida da esquerda para a direita ou da direita para a esquerda, ex:
 # “arara” – observe que a string vazia é dada por "" ao invés de [ ] ).
+
+def reverteString(string):
+    revertida = string[::-1]
+    return revertida
+
+def éPalíndromo(string):
+    revertida = reverteString(string)
+    if string == revertida:
+        return True
+    else:
+        return False
